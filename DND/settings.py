17c457e26bdb57d4fd82187fd7f423ae76f04b22
KoTeD0 <darkest.jerry@gmail.com>
2022-04-16 02:41:24 +0300

    initial

--- DND/settings.py
+++ DND/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'users',
+    'board',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -57,7 +60,7 @@ ROOT_URLCONF = 'DND.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -109,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -125,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom settings
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
---
+++ board/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
+
+def index(request):
+    """Home page"""
+    return render(request, 'board/index.html')
