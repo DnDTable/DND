d533f2411a1b80da6e71adc53d47ad4acbd426a5
kotedo <darkest.jerry@gmail.com>
2022-05-16 19:51:41 +0300

    added dockerized postgres db support

--- DND/settings.py
+++ DND/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'DND.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_postgres_extensions.backends.postgresql',
+        'NAME': 'demo',
+        'USER': 'demo',
+        'PASSWORD': 'demo',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
