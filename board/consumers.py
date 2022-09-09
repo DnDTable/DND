2e68a3df523920ac071c7944c23aec574bf53e5e
KoTeDo <darkest.jerry@gmail.com>
2022-09-09 14:36:26 +0300

    basic socket receive method added

--- board/consumers.py
+++ board/consumers.py
@@ -1,6 +1,7 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from datetime import datetime
 
 class TableConsumer(WebsocketConsumer):
     def connect(self):
@@ -9,4 +10,12 @@ class TableConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({
             'type': 'connection_established',
             'message': 'You are now connected'
+        }))
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+
+        self.send(text_data=json.dumps({
+            'type':'message',
+            'message': datetime.now()
         }))
