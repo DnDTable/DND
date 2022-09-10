9c2b63068d23515b97b6d2fe025fc02e0a0865a5
Laterner <ivantrofimov2001@gmail.com>
2022-09-10 19:52:42 +0300

    add nicknames

--- board/consumers.py
+++ board/consumers.py
@@ -1,11 +1,29 @@
 import json
+from mimetypes import init
 from time import time
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from datetime import datetime
+import random
 
+nick_names = [
+    'Пипидастр',
+    'Юсуф',
+    'Маркалей',
+    'Гусь',
+    'Кирпич',
+    'Медведь',
+    'Олень',
+    'Реджеп',
+    'Ягода',
+    ]
 
 class ChatConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.usernames = nick_names
+        self.current_names = []
+
     def connect(self):
         self.room_group_name = 'test'
         async_to_sync(self.channel_layer.group_add)(
@@ -14,11 +32,15 @@ class ChatConsumer(WebsocketConsumer):
         )
 
         self.accept()
-
+        user = self.usernames[random.randint(0, len(self.usernames) - 1)]
+        # self.usernames.pop(self.usernames.index(user))
+        
         self.send(text_data=json.dumps({
             'type': 'connection_status',
-            'message': 'Выподключены с серверу'
+            'message': f'Выподключены с серверу. Ваш ник - {user}',
+            'user': user
         }))
+        self.send_room("Пользователь <b style='color: green;'>" + user + "</b> присоединился к чату", user)
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
