077bb71b35c06d5b8aa394381461db0c2c0b0d5e
Laterner <ivantrofimov2001@gmail.com>
2022-09-28 00:27:12 +0300

    fix server & table

--- board/consumers.py
+++ board/consumers.py
@@ -6,27 +6,35 @@ import json
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = self.room_name
-        self.user = User.username
+        self.room_name = 'lobby'#self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'lobby' #'chat_%s' % self.room_name
+        self.user = str(User.username) 
 
+        print('User is: ', type(self.user))
+        
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
+        
+        # await self.send_to_room('lobby', _type='connection_status', message='User <b style="color: green;">' + self.user + '</b> connected', user=self.user)
 
-        await self.send_to_room(_type='connection_status', message='User <b style="color: green;">' + self.user + '</b> connected', user=self.user)
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'type': 'connection_status',
+            'message': 'User <b style="color: green;">' + 'self.user' + '</b> connected',
+            'user': 'User',
+        }))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
-    async def send_to_room(self, channel_name, _type='sys', message='null', user='sys', ):
+    async def send_to_room(self, channel_name='lobby', _type='sys', message='null', user='sys'):
         await self.channel_layer.group_send(
-            channel_name | self.room_name,
+            channel_name,
             {
                 'type': 'room_event',
                 '_type': _type,
@@ -34,12 +42,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 'user': user
             }
         )
-
+    
     async def room_event(self, event):
         await self.send(text_data=json.dumps({
-            'type': event['_type'],
-            'message': event['message'],
-            'user': event['user'],
+            'type': str(event['_type']),
+            'message': str(event['message']),
+            'user': str(event['user']),
         }))
 
     async def receive(self, text_data=None, bytes_data=None):
@@ -49,18 +57,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self._type: str = text_data_json['type']
             self._message: str = text_data_json['message']
             self._user: str = text_data_json['user']
-        except json.JSONDecodeError as exception:
+
+            print('type:', f"'{self._type}'", 'message:', f"'{self._message}'", 'user:', f"'{self._user}'")
+
+            match self._type:
+                case 'message':
+                    await self.send_to_room(_type=self._type, message=self._message, user=self._user)
+                case 'answer':
+                    await self.send(text_data=json.dumps({'type': self._type, 'message': self._message, 'user': self._user}))
+                case 'move':
+                    await self.send_to_room(_type=self._type, message=self._message, user=self._user)
+
+        except Exception as exception:
             await self.send(text_data=json.dumps({
                 'error', exception
             }))
-
-        match self._message:
-            case 'message':
-                await self.send_to_room(_type=self._type, message=self._message, user=self._user)
-            case 'answer':
-                await self.send(text_data={self._type, self._message, self.user})
-            case 'move':
-                await self.send_to_room(_type=self._type, message=self._message, user=self._user)
 
 
 
