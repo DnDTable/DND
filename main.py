39338084d9fd6a26a63b23f9593d66ce4eb55921
Laterner <55022049+Laterner@users.noreply.github.com>
2022-11-15 15:13:00 +0300

    add queue in consumers

---
+++ main.py
@@ -0,0 +1,16 @@
+from itertools import cycle
+
+
+c = [1, 2, 3, 4]
+
+p = cycle(c)
+
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
+print(next(p))
